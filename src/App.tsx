e7ba6f56a6871067cb846fe4dbe8db896325b7a3
Cyril Gourgouillon <cyrilgour@gmail.com>
2023-11-21 09:49:45 -0500

    Add basic responsivity

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ const App = () => {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       <div className="flex flex-col items-center">
-        <div className="text-lg font-semibold">RANDOM NOTE GENERATOR</div>
-        <div className="flex flex-row gap-7 text-[45px] mt-6 mb-6 font-bold">
+        <div className="text-lg font-semibold">RANDOM NOTES GENERATOR</div>
+        <div className="flex flex-col md:flex-row md:gap-7 text-[35px] md:text-[45px] mt-6 mb-6 font-bold">
           {notes.map((note: Note, index: number) => (
             <div key={index}>{Notes[note]}</div>
           ))}

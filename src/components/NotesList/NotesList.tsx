2f4034c14ec6fd6e62315261c5337fcbe33264c4
Cyril Gourgouillon <cyrilgour@gmail.com>
2023-11-22 17:39:05 -0500

    increase size

--- src/components/NotesList/NotesList.tsx
+++ src/components/NotesList/NotesList.tsx
@@ -10,7 +10,7 @@ export const NotesList = ({
   return (
     <>
       <div className="text-[25px] md:text-[25px] font-bold text-neutral-500">{GuitarStringDecorator}</div>
-      <div className="flex flex-col md:flex-row md:gap-7 text-[35px] md:text-[45px] mb-6 font-bold">
+      <div className="flex flex-col md:flex-row md:gap-7 text-[35px] md:text-[75px] mb-6 font-bold">
         {notes.map((note: Note, index: number) => (
           <div key={index}>{note}</div>
         ))}

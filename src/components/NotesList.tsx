75aa9ac65d1013f07ac4c452e15b8e6f34e038f2
Cyril Gourgouillon <cyrilgour@gmail.com>
2023-11-26 22:30:58 -0500

    App redesign

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,4 +1,4 @@
-import { Note } from '../config';
+import { Note } from "../config";
 
 export const NotesList = ({
   notes,
@@ -11,10 +11,12 @@ export const NotesList = ({
 }) => {
   return (
     <>
-      <div className="text-[25px] md:text-[25px] font-bold text-neutral-500">{GuitarStringDecorator}</div>
+      <div className="text-[25px] md:text-[25px] font-bold text-neutral-500">
+        {GuitarStringDecorator}
+      </div>
       <div
         onClick={onClick}
-        className="flex flex-col md:flex-row md:gap-7 text-[35px] md:text-[75px] mb-6 font-bold cursor-pointer select-none"
+        className="flex flex-col flex-wrap md:flex-row md:gap-7 text-[35px] md:text-[75px] mb-6 font-bold cursor-pointer select-none"
       >
         {notes.map((note: Note, index: number) => (
           <div key={index}>{note}</div>
--- src/pages/ChordsPage.tsx
+++ src/pages/ChordsPage.tsx
@@ -1,17 +1,26 @@
-import { ChordsList, ChordsSettings } from '../components';
+import { ChordsList, ChordsSettings } from "../components";
 
-import { useChordSettingsContext } from '../hooks';
+import { useChordSettingsContext } from "../hooks";
 
 export const ChordsPage = () => {
-  const { chords, isShapeVisible, cagedPosition, getRandomChordsOnClick} = useChordSettingsContext();
+  const { chords, isShapeVisible, cagedPosition, getRandomChordsOnClick } =
+    useChordSettingsContext();
 
-  const ShapeDecorator: React.ReactNode = <div className={`${isShapeVisible ? '' : 'invisible'}`}>{cagedPosition}</div>;
+  const ShapeDecorator: React.ReactNode = (
+    <div className={`${isShapeVisible ? "" : "invisible"}`}>
+      {cagedPosition}
+    </div>
+  );
 
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <div className="w-screen flex items-center justify-center">
         <div className="flex flex-col items-center">
-          <ChordsList chords={chords} ShapeDecorator={ShapeDecorator} onClick={getRandomChordsOnClick} />
+          <ChordsList
+            chords={chords}
+            ShapeDecorator={ShapeDecorator}
+            onClick={getRandomChordsOnClick}
+          />
           <ChordsSettings />
         </div>
       </div>

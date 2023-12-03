2c5feedff86d062247ee5ccc1450c3b38891f15d
Cyril Gourgouillon <cyrilgour@gmail.com>
2023-12-02 21:00:22 -0500

    fix suffle

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -4,18 +4,26 @@ import { NOTES_LIST_MAX, NOTES_LIST_MIN } from "../config/constants";
 import { getRandomItemFrom } from "../utils/random";
 import { shuffle } from "../utils/shuffle";
 
-export const getListOfRandomNotes = (count: number, allNotes?: Note[]): Note[] => {
+export const getListOfRandomUniqueNotes = (count: number, allNotes?: Note[]): Note[] => {
   const randomNotes = shuffle(allNotes ?? notes);
 
   return randomNotes.slice(undefined, count);
 };
 
+export const getListOfRandomNotes = (count: number, allNotes?: Note[]): Note[] => {
+  const randomNotes: Note[] = [];
+  for(let i = 0; i < count; i++) {
+    randomNotes.push(getRandomNote(allNotes));
+  }
+  return randomNotes;
+}
+
 export const isValidNoteCountList = (count: number): boolean => {
   return count >= NOTES_LIST_MIN && count <= NOTES_LIST_MAX
 }
 
-export const getRandomNote = (): Note => {
-  return getRandomItemFrom(notes);
+export const getRandomNote = (allNotes?: Note[]): Note => {
+  return getRandomItemFrom(allNotes ?? notes);
 };
 
 export const getRandomNoteFromCaged = (): Caged => {

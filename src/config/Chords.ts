0729d368f8bd169e5a271ce9aa9403ae123ee268
Cyril Gourgouillon <cyrilgour@gmail.com>
2024-10-28 14:25:45 -0400

    Add dom7 chords + more visibility (#35)

--- src/config/Chords.ts
+++ src/config/Chords.ts
@@ -1,6 +1,6 @@
 import { Note } from '.';
 
-export const chordTypes = ['M', 'm', 'maj7', 'min7'] as const;
+export const chordTypes = ['M', 'm', 'maj7', 'min7', 'dom7'] as const;
 
 export type ChordType = (typeof chordTypes)[number];
 
